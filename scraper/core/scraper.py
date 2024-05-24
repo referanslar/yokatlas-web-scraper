26971ca4d1881e02b04de345ccca3702ae02590a
referanslar <147508935+referanslar@users.noreply.github.com>
2024-05-24 14:04:15 +0300

    feat: create `scraper` class for getting source code with `BeautifulSoup`

---
+++ scraper/core/scraper.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from bs4 import BeautifulSoup
+import requests, time
+
+class Scraper:
+
+    def soupWithoutSelenium(self, url):
+        res = requests.get(url)
+        return BeautifulSoup(res.content, "html.parser")
+
+    def soup(self, url):
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        driver = webdriver.Chrome(options=chrome_options)
+        driver.get(url)
+
+        WebDriverWait(driver, 10).until(
+            lambda driver: driver.execute_script("return document.readyState") == "complete"
+        )
+
+        time.sleep(5)
+
+        return BeautifulSoup(driver.page_source, "html.parser")
